339b0567815306a15a062b5863c210e002b78d15
ettom <36895504+ettom@users.noreply.github.com>
2022-02-25 17:34:37 +0200

    Add flag to set email

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 type options struct {
-	format, out, in string
+	format, out, in, email string
 	privateKey      bool
 }
 
@@ -24,6 +24,7 @@ func parseFlags(args []string) options {
 	f.StringVar(&opts.format, "format", "armor", "GPG format encoding (binary|armor)")
 	f.StringVar(&opts.in, "i", "-", "Input path. Reads by default from standard output")
 	f.StringVar(&opts.out, "o", "-", "Output path. Prints by default to standard output")
+	f.StringVar(&opts.email, "email", "root@localhost", "Email to set for the PGP user id")
 	if err := f.Parse(args[1:]); err != nil {
 		// should never happen since flag.ExitOnError
 		panic(err)
@@ -68,7 +69,7 @@ func convertKeys(args []string) error {
 		}
 	}
 
-	gpgKey, err := SSHPrivateKeyToPGP(sshKey)
+	gpgKey, err := SSHPrivateKeyToPGP(sshKey, opts.email)
 	if err != nil {
 		return err
 	}
